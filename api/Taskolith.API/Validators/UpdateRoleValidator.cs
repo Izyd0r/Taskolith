d14f09a7462ef289c319eeca711c409fc235893c
unknown <izydor4678@gmail.com>
2025-07-06 18:45:54 +0200

    Add endpoint that updates role

--- api/Taskolith.API/Validators/UpdateRoleValidator.cs
+++ api/Taskolith.API/Validators/UpdateRoleValidator.cs
@@ -1,20 +1,18 @@
 using FluentValidation;
 using Taskolith.API.Data.Types;
-using Taskolith.API.OrganizationManagement.Roles;
 using Taskolith.API.OrganizationManagement.Roles.Requests;
 
 namespace Taskolith.API.Validators;
 
-public class CreateRoleRequestValidator : AbstractValidator<CreateRoleRequest> {
-    public CreateRoleRequestValidator() {
-        RuleFor(x => x.Name)
-            .NotNull().WithMessage("Name is required")
-            .NotEmpty().WithMessage("Name is required")
-            .MaximumLength(100).WithMessage("Name must not exceed 100 characters");
-        RuleFor(x => x.Permissions)
-            .Must(BeAValidPermission).WithMessage("Invalid permission flags"); 
+public class UpdateRoleValidator : AbstractValidator<UpdateRoleRequest> {
+    public UpdateRoleValidator() {
+       RuleFor(request => request.Name)
+           .NotEmpty().WithMessage("Name is required")
+           .MaximumLength(100).WithMessage("Name cannot exceed 100 characters");
+       RuleFor(request => request.Permissions)
+           .Must(BeAValidPermission).WithMessage("Invalid permission flags");
     }
-    
+
     private bool BeAValidPermission(Permission permission) {
         var allDefined = Enum.IsDefined(typeof(Permission), permission);
         if (allDefined) return true;
