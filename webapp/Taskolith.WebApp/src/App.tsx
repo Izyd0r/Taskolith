9c89aaaa8f96ee0893ffd6b0dbe329865a30f3e5
izydor <izydor4678@gmail.com>
2025-07-20 15:31:50 +0200

    Add representation of dashboard main page with subpages

--- webapp/Taskolith.WebApp/src/App.tsx
+++ webapp/Taskolith.WebApp/src/App.tsx
@@ -2,18 +2,28 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from '@/features/landing-page/pages/LandingPage';
 import LoginPage from '@/features/auth/pages/LoginPage';
 import SignupPage from '@/features/auth/pages/SignupPage';
-import DashboardPage from '@/features/dashboard/pages/DashboardPage';
+
+import DashboardLayout from '@/features/dashboard/layout/DashboardLayout'
+import DashboardHome from '@/features/dashboard/components/DashboardHome';
+import DashboardTasks from '@/features/dashboard/components/DashboardTasks';
+import DashboardOrganisation from '@/features/dashboard/components/DashboardOrganisation';
+import DashboardMyInvites from '@/features/dashboard/components/MyInvites';
 
 function App() {
-  return (
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        {/*other routes*/}
-      </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/dashboard" element={<DashboardLayout />}>
+                <Route index element={<DashboardHome />} />
+                <Route path="tasks" element={<DashboardTasks />} />
+                <Route path="create-organisation" element={<DashboardOrganisation />} />
+                <Route path="invites" element={<DashboardMyInvites />} />
+            </Route>
+            {/*other routes*/}
+        </Routes>
+    )
 }
 
 export default App;
