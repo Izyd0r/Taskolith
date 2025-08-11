e4af8e270bec5c360795a702919f26e252dcffac
Izydor <izydor4678@gmail.com>
2025-08-11 14:06:22 +0200

    Update projects page

--- webapp/Taskolith.WebApp/src/features/organisation/api/GetProjects.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from '@/lib/axios'
-import { type Project } from '@/features/organisation/types/Project'
-
-export const GetProjects = async (organisationId: string): Promise<Project[]> => {
-    const { data } = await apiClient.get(`/organisations/${organisationId}/projects`)
-    return data
-}
---
+++ webapp/Taskolith.WebApp/src/features/organisation/api/Projects.ts
@@ -0,0 +1,64 @@
+import { apiClient } from '@/lib/axios'
+import { type CreateProjectRequest } from '@/features/organisation/types/CreateProjectRequest'
+import { type Project } from '@/features/organisation/types/Project'
+
+export const CreateProject = async (
+    organisationId: string,
+    request: CreateProjectRequest
+) => {
+    const { data } = await apiClient.post(`/organisations/${organisationId}/projects`, request)
+    return data
+}
+
+export const GetProjects = async (organisationId: string): Promise<Project[]> => {
+    const { data } = await apiClient.get(`/organisations/${organisationId}/projects`)
+    return data
+}
+
+export const UpdateProject = async (
+    organisationId: string,
+    projectId: string,
+    payload: { name?: string; description?: string }
+) => {
+    try {
+        const { data } = await apiClient.put(
+            `/organisations/${organisationId}/projects/${projectId}`,
+            payload
+        )
+        return data
+    } catch (error: any) {
+        if (error.response && error.response.status === 204) {
+            return { success: true }
+        }
+        throw error
+    }
+}
+
+export const DeleteProject = async (
+    organisationId: string,
+    projectId: string
+) => {
+    await apiClient.delete(`/organisations/${organisationId}/projects/${projectId}`)
+}
+
+export const AssignMembersToProject = async (
+    organisationId: string,
+    projectId: string,
+    membersId: string[]
+) => {
+    await apiClient.post(
+        `/organisations/${organisationId}/projects/${projectId}/members`,
+        { membersId }
+    )
+}
+
+export const RemoveMemberFromProject = async (
+    organisationId: string,
+    projectId: string,
+    memberId: string
+) => {
+    await apiClient.delete(
+        `/organisations/${organisationId}/projects/${projectId}/members/${memberId}`
+    )
+}
+
